bcb36bba213a0726319fff99c4826dc11add868c
yoga <yogaagle123z@gmail.com>
2023-06-22 22:13:29 +0700

    pertama

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = () => {
+    return (
+        <div className="w-full bg-red-200 flex h-screen justify-center items-center">
+            <h1>Index</h1>
+        </div>
+    );
+};
+
+export default Index;

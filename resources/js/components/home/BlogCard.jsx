e145858ab8f52804efa5a95d988fba41eb6bda41
yoga <yogaagle123z@gmail.com>
2023-10-31 14:45:29 +0700

    update

--- resources/js/components/home/BlogCard.jsx
+++ resources/js/components/home/BlogCard.jsx
@@ -1,11 +1,14 @@
 import { Link } from "@inertiajs/react";
 import React from "react";
+import parse from "html-react-parser";
 
 const BlogCard = ({blogs}) => {
     return (
         <div className="lg:px-40 lg:py-9 md:px-20 md:py-6 px-4 py-4 flex flex-wrap gap-4">
-            <div className="w-full flex flex-col justify-center items-center mb-10 lg:mb-10 md:mb-10"
-            data-aos="zoom-in-down">
+            <div
+                className="w-full flex flex-col justify-center items-center mb-10 lg:mb-10 md:mb-10"
+                data-aos="zoom-in-down"
+            >
                 <h1 className="lg:text-5xl md:text-4xl text-3xl font-bold text-green-custom">
                     POSTINGAN <span className="text-black">TERBARU</span>
                 </h1>
@@ -13,32 +16,41 @@ const BlogCard = ({blogs}) => {
             </div>
 
             <div className="flex w-full justify-center gap-4 flex-wrap md:h-auto">
-
-                {blogs && blogs.map((item, index) => (
-                    <div key={index} className="card lg:w-[30%] md:w-[30%] w-full shadow-xl rounded-none md:h-full"
-                    data-aos="zoom-in-down
-                    ">
-                        <figure>
-                            <img src={item.image} alt="" />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title">{item.title}</h2>
-                            <div className="card-actions">
-                                {item.tags.map((tag, index) => (
-                                <Link href={`/blog/tag/${tag}`} key={index} className="badge badge-outline">
-                                    {tag}
-                                </Link>
-                                ))}
-                            </div>
-                            <p className="w-full line-clamp-4">{item.content}</p>
-                            <div className="text-blue-700 cursor-pointer">
-                                <Link href={`/blog/spesifik/${item.id}`}>
-                                    <h1>Baca Selengkapnya</h1>
-                                </Link>
+                {blogs &&
+                    blogs.map((item, index) => (
+                        <div
+                            key={index}
+                            className="card lg:w-[30%] md:w-[30%] w-full shadow-xl rounded-none md:h-full"
+                            data-aos="zoom-in-down
+                    "
+                        >
+                            <figure>
+                                <img src={item.image} alt="" />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{item.title}</h2>
+                                <div className="card-actions">
+                                    {item.tags.map((tag, index) => (
+                                        <Link
+                                            href={`/blog/tag/${tag}`}
+                                            key={index}
+                                            className="badge badge-outline"
+                                        >
+                                            {tag}
+                                        </Link>
+                                    ))}
+                                </div>
+                                <div className="w-full line-clamp-4">
+                                    {parse(item.content)}
+                                </div>
+                                <div className="text-blue-700 cursor-pointer">
+                                    <Link href={`/blog/spesifik/${item.id}`}>
+                                        <h1>Baca Selengkapnya</h1>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
             </div>
         </div>
     );
--- resources/js/components/home/OurBlog.jsx
+++ resources/js/components/home/OurBlog.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "@inertiajs/react";
+import parse from "html-react-parser";
 
 
 export default function OurBlog({lastBlog}) {
 
     return (
         <>
-            <div className="py-6 px-4 lg:px-40 md:px-20 bg-gray-200"
-            >
+            <div className="py-6 px-4 lg:px-40 md:px-20 bg-gray-200">
                 <div className="flex flex-col items-center justify-center gap-8 ">
                     <div
                         className="flex w-full flex-col  lg:text-5xl md:text-4xl text-3xl font-bold items-center mb-6"
@@ -21,8 +21,10 @@ export default function OurBlog({lastBlog}) {
                     <div className="flex flex-col md:flex-row items-center justify-center mb-6">
                         <div className="flex flex-col lg:flex-row md:flex-row w-full h-full justify-center lg:gap-20 md:gap-10 ">
                             {/* Image */}
-                            <div className="flex flex-col h-auto relative justify-center md:w-[50%] lg:w-[50%]"
-                            data-aos="zoom-in-down">
+                            <div
+                                className="flex flex-col h-auto relative justify-center md:w-[50%] lg:w-[50%]"
+                                data-aos="zoom-in-down"
+                            >
                                 <div className="w-full h-full flex justify-center items-center px-6">
                                     <img
                                         className="w-full"
@@ -49,9 +51,9 @@ export default function OurBlog({lastBlog}) {
                                         {lastBlog.title}
                                     </h1>
                                 </div>
-                                <p className="text-justify indent-12 font-medium lg:text-xl md:text-lg line-clamp-3">
-                                    {lastBlog.content}
-                                </p>
+                                <div className="text-justify indent-12 font-medium lg:text-xl md:text-lg line-clamp-3">
+                                    {parse(lastBlog.content)}
+                                </div>
                                 <div className="flex w-full h-full justify-start  lg:w-[80%] md:py-7 py-3">
                                     <Link
                                         href={`/blog/spesifik/${lastBlog.id}`}
